844d11b8d29dca8930178b1efac714c002289d6f
Bianca Medeiros <biancamm0107@gmail.com>
2024-06-25 09:32:14 -0300

    Controller constructor and methods

--- src/snakeGame.h
+++ src/snakeGame.h
@@ -49,11 +49,66 @@ private:
 
 public:
     void read_config(std::string path);
-    // Data(const std::string& iniFile) : iniPath(iniFile) {read_ini(iniFile);}
-    // void read_ini(std::string iniFile);// Getter for variablesAndValues
-    // const std::unordered_map<std::string, std::string>& get_variablesAndValues() const {
-    //     return variablesAndValues;
-    // }
+
+    Controller(const Data& data) {
+        // Initialize member variables using the Data object
+        const auto& config = data.get_variablesAndValues();
+
+        if (config.find("input_cfg") != config.end()) {
+            cfgFile = config.at("input_cfg");
+            if(cfgFile.length() >=2 && cfgFile.front() == '"'  && cfgFile.back() == '"'){
+                cfgFile = cfgFile.substr(1, cfgFile.length() - 2);
+            }
+            read_config(cfgFile);
+        }
+        if (config.find("levels") != config.end()) {
+            numberOfLevels = std::stoi(config.at("levels"));
+        }
+        if (config.find("fps") != config.end()) {
+            fps = std::stoi(config.at("fps"));
+        }
+        if (config.find("snake_lives") != config.end()) {
+            snakeLives =  std::stoi(config.at("snake_lives"));
+        }
+        if (config.find("food") != config.end()) {
+            totalFood =  std::stoi(config.at("food"));
+        }
+    }
+
+    //=== Common methods for the Game Loop design pattern.
+    
+    /// Process user input events, depending on the current game state.
+    void process_events();
+    /// Update the game based on the current game state.
+    void update();
+    /// Renders the game to the user.
+    void render() const;
+
+    /// Returns true when the user wants to quit the game.
+    bool game_over;
+
+private:
+    /// Reads the user confirmation, enter.
+    bool read_user_enter() const;
+
+    /// Show the welcome message.
+    void display_welcome() const;
+    /// Show game screen
+    void display_running()const;
+    /// Show level up message
+    void display_levelup()const;
+    /// Show won message
+    void display_won()const;
+    /// Show lost message
+    void display_lost()const;
+    /// Show options
+    void show_game_options()const;
+    /// Show press enter
+    void show_enter()const;
+
+    /// Changes the state of the game.
+    void change_state(game_state_e new_state);
 };
+
 
 #endif
