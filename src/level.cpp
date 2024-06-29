fbb61b00dad3813836605205c424e49202d2e2aa
Bianca Medeiros <biancamm0107@gmail.com>
2024-06-29 05:03:43 -0300

    random foood location

--- src/level.cpp
+++ src/level.cpp
@@ -4,15 +4,29 @@
 #include <iostream>
 #include <time.h>
 #include <utility>
+#include <algorithm>    // std::replace
 
 #include "level.h"
+#include "snake.h"
 
-void Level::set_random_food_location(){
+void Level::randomly_place_foood(Snake& snake){
+    int rows = static_cast<int>(m_levelMaze.size());
+    int cols = static_cast<int>(m_levelMaze[0].size());
+    std::vector<std::pair<int, int>> available_coords;
+
+    for(int ii = 0; ii < rows; ii++){
+        for(int jj = 0; jj < cols; jj++){
+            if(m_levelMaze[ii][jj] == ' ' && !snake.check_coord_in_body(std::make_pair(ii,jj))){
+                available_coords.push_back(std::make_pair(ii,jj));
+            }
+        }
+    }
+
     srand(time(0));
 
-    int numberOfPossibleLocations = static_cast<int>(m_emptyLocations.size());
+    int randomIndex = rand() % static_cast<int>(available_coords.size());
 
-    int locationIndex = rand() % numberOfPossibleLocations;
+    std::pair<int, int> foodCoord = available_coords[randomIndex];
 
-    m_foodLocation = m_emptyLocations[locationIndex];
+    m_levelMaze[foodCoord.first][foodCoord.second] = '9';
 }
