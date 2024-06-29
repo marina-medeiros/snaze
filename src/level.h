c87441b010feae41094d4b417dcc4c5e179fc342
Bianca Medeiros <biancamm0107@gmail.com>
2024-06-29 05:00:37 -0300

    spawn

--- src/level.h
+++ src/level.h
@@ -4,6 +4,8 @@
 #include <string> // std::string
 #include <vector>
 
+class Snake;
+
 class Level{
 private:
     int m_rows;
@@ -11,9 +13,10 @@ private:
     int m_levelNumber;
     char m_wall = '#';
     char m_invisibleWall = '.';
-    char m_spawnLocation = '&';
+    char m_spawn = '&';
     char m_emptySpace = ' ';
     std::pair<int, int> m_foodLocation;
+    std::pair<int, int> m_spawnLocation;
     std::vector<std::vector<char>> m_levelMaze;
     std::vector<std::pair<int, int>> m_emptyLocations;
 
@@ -22,15 +25,17 @@ public:
     int get_cols(){return m_cols;}
     char get_wall(){return m_wall;}
     char get_invisibleWall(){return m_invisibleWall;}
-    char get_spawnLocation(){return m_spawnLocation;}
+    char get_spawn(){return m_spawn;}
     char get_emptySpace(){return m_emptySpace;}
     int get_levelNumber(){return m_levelNumber;}
+    std::pair<int, int> get_spawnLocation(){return m_spawnLocation;}
     std::pair<int, int> get_foodLocation(){return m_foodLocation;}
     std::vector<std::vector<char>> get_levelMaze(){return m_levelMaze;}
 
     void set_rows(int r){m_rows = r;}
     void set_cols(int c){m_cols = c;}
     void set_levelMaze(std::vector<std::vector<char>> maze){m_levelMaze = maze;}
+    void add_spawnLocation(std::pair<int, int> spawnloc){m_spawnLocation = spawnloc;}
     void add_emptyLocation(std::pair<int, int> loc){m_emptyLocations.push_back(loc);}
 
     void set_random_food_location();
