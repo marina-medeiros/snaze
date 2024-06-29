16078079eee6defdce2d10d3101a61b79429f137
Bianca Medeiros <biancamm0107@gmail.com>
2024-06-29 05:06:56 -0300

    snake ate check

--- src/snake.cpp
+++ src/snake.cpp
@@ -2,8 +2,8 @@
 #include <vector>
 #include <deque>
 
-#include "player.h"
 #include "snake.h"
+#include "level.h"
 
 bool Snake::check_coord_in_body(std::pair<int, int> coord){
     for(auto& cell : m_body){
@@ -15,8 +15,15 @@ bool Snake::check_coord_in_body(std::pair<int, int> coord){
 }
 
 //precisam do mapa:
-bool Snake::snake_ate_check(){
-    
+bool Snake::snake_ate_check(Level &level) {
+    int headRow = m_headLocation.first;
+    int headCol = m_headLocation.second;
+    if(level.get_levelMaze()[headRow][headCol] == '9') {
+        m_size++;
+        level.randomly_place_foood(*this);
+        return true;
+    }
+    return false;
 }
 
 void Snake::update_snake_isAlive(){
